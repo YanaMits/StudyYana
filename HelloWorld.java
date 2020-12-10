7a1bcaa153b3a28cc0f31bb8f814e662e8e7d3d0
Yana <yana.pressa@gmail.com>
2020-12-10 15:34:51 +0100

    my first code

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class HelloWorld {
+
+    public static void HelloWorld(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Thank you, Aleshkov!");
+    }
+}
